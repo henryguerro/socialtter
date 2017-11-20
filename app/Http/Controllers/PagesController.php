e8d371c4ec47386cd9782b65755d523786267d73
Henry Palacios <henrydpalacios@gmail.com>
2017-11-20 00:04:48 -0300

    Add Auth, change of Bootstrap3 to Bootstrap4

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,7 +9,7 @@ class PagesController extends Controller
 {
     public function home()
     {
-        $messages = Message::paginate(2);
+        $messages = Message::paginate(4);
         return view('welcome', compact('messages'));
     }
 }
